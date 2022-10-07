3a070082f1c721df7dd682c2e25c7265d6e379d2
Mat Taylor <mat@MacBook-Pro-2.local>
2022-10-06 17:35:04 -0700

    clone log

--- objix.js
+++ objix.js
@@ -111,7 +111,7 @@ P.bind = function(key, fn) {
 }
 
 P.log = function(msg='', c='log') {
-  console[c](new Date().toISOString().slice(0,-8), msg, this)
+  console[c](new Date().toISOString().slice(0,-8), msg, this.clone(-1))
   return this
 }
 
