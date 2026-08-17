bcd985ddc1c5b749eb461bfba5ce1fe24cc294d2
Mat Taylor <mat.taylor@omc.com>
2026-08-17 13:37:38 -0700

    safer _eval - no imports

--- objix.js
+++ objix.js
@@ -155,7 +155,7 @@ const
   },
 
   eval(s) {
-    const g = { Math: Math, RegExp: RegExp, Date: Date, JSON: JSON }._map(_ => O.freeze(_))
+    const g = { Math, RegExp, Date, JSON }._map(_ => O.freeze(_))
     return /\bimport\b/.test(s) ? 'invalid' : Function('p', `with (p) { return ${s} }`)(
       new Proxy(this, {
         has() { return true },
