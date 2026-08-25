866c3ef0c5be8210c7438a1208b520a81d0169dd
Mat Taylor <mat.taylor@omc.com>
2026-08-24 22:41:53 -0700

    terse

--- objix.js
+++ objix.js
@@ -69,16 +69,12 @@ const
   },
 
   clone(d, e) {
-    return !this._is(O)
-      ? this.valueOf()
-      : (!e && d == -1)
-        ? this._try(structuredClone, () => this._clone(d, 1))
-        : !this._len()
-          ? this.map ? this : new this[C](this)
-          : d
-            ? this._map(v => v?._clone(d - 1) ?? v)
-            : this.map ? [ ...this ] : { ...this }
-    },
+    return !this._is(O) ? this.valueOf()
+      : !e && d === -1 ? this._try(structuredClone, () => this._clone(d, 1))
+      : !this._len() ? this.map ? this : new this[C](this)
+      : d ? this._map(v => v?._clone(d - 1) ?? v)
+      : this.map ? [...this] : { ...this }
+  },
 
   join(...a) {
     let r,o,k
